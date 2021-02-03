4f259b2fa0b7ba09b57c6c7ef614a68f789b6514
HamzeOmar <omarh9358@@gmail.com>
2021-02-03 00:50:33 -0600

    Lab 3 commit

---
+++ R.script.R
@@ -0,0 +1,49 @@
+# load auk package
+
+library(auk)                          # load the auk package
+birds <- ebird_taxonomy %>%           # start with the ebird_taxonomy data
+  as_tibble() %>%                     # tibbles print better in the console
+  filter(category == "species")       # remove non-species taxa
+
+# birds data
+
+birds
+
+distinct(birds, order)
+
+count(birds, order)
+
+# Create canvas for the graph
+
+ggplot(data = birds)
+
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = order))
+
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)))
+
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)), fill = "#C5351B") 
+
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)), fill = "#C5351B") +
+  labs(x = "Order", x = "Frequency (number of species)")
+
+# Final graph 
+
+ggplot(data = birds) +
+  geom_bar(mapping = aes(x = fct_infreq(order)), fill = "#C5351B", 
+           width = 0.4) +
+  labs(x = "Order", y = "Frequency (number of species)") +
+  #scale_y_continuous(limits =  c(0, NA), expand = expansion(mult = 0.4)) +
+  theme_classic(base_size = 13) +
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(color = "black", size = rel(.8)),
+    axis.text.x = element_text(angle = 50 , hjust = 1),
+    axis.ticks.x = element_blank()
+  ) + 
+  scale_y_log10(expand = expansion(mult = 0))
+
+
---
+++ README.md
@@ -0,0 +1,47 @@
+Lab 3: Introduction to ggplot
+================
+Hamze Omar
+2021-02-03
+
+> INSTRUCTIONS: REMOVE THIS LINE AND REPLACE \_\_\_\_ WITH YOUR OWN TEXT
+
+Welcome to my Lab 3 repository for Quantitative Biology BIOL 275 at
+MSUM.
+
+## Lab objectives
+
+The objectives of this lab are:
+
+  - Use the layered grammar of graphics:
+
+  - Create a blank graph using the ggplot() function
+
+  - Add geometric objects to a graph
+
+  - Define an aesthetic mapping for a graph
+
+  - Add labels to a graph
+
+  - Fix some common styling
+
+  - Create a graph showing the distribution of a single variable
+
+\-Create a bar chart for a categorical variable using the geom\_col()
+and geom\_bar() functions - Arrange the categories in a bar chart by
+frequency - Create a histogram for a numerical variable using the
+geom\_histogram() function - Choose and set appropriate histogram bin
+widths
+
+## Repository contents
+
+The repository contains the following scripts:
+
+  - [Lab Report 3](lab-report.md)
+
+  - fireflies\_data
+
+  - Lab 3 script
+
+  - 
+    
+    -----
